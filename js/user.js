8f566896463c88720dc932459c3e29bf26103b62
Peter Fischer <1910653356@stud.fh-kufstein.ac.at>
2020-03-19 16:34:08 +0100

    UE 5 fertig

--- js/user.js
+++ js/user.js
@@ -1,12 +1,8 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
     var userListBody = $('.userList tbody');
+    var userNumber = 1;
 
-    //@todo store and somehow update the current number of users
-
-
-    $('.needs-validation').submit(function(event) {
-
+    $('.needs-validation').submit(function (event) {
         event.preventDefault();
         event.stopPropagation();
 
@@ -17,30 +13,20 @@ $(document).ready(function() {
             return false;
         }
 
-        //@todo
-        //1. get values
-        //2. create a new element
-        //3. somehow add them to userListBody
-        //4. update number of current users
-        //5. clear entries from the form
-        //6. maybe do something else... :-)
-
-        //your code follows here
-
+        userNumber++;
+        var username = $('#username').val();
+        userListBody.append('<tr><td>' + userNumber + '</td><td>' + username + '</td><td><button type="button" class="btn btn-secondary btn-danger deleteTrigger" title="Löschen"><i class="fa fa-trash"></i></button></td>');
+        $('.needs-validation').trigger("reset");
         return false;
     });
 
 
-    $('.deleteTrigger').click(function() {
-        //@todo
-        //1. remove current user from dom
-        //2. update number of current users
-
-        //your code follows here
-
-
+    $('.deleteTrigger').click(function () {
+        if (confirm("wirklich Löschen?")) {
+            $(this).parents("tr").remove();
+            userNumber--;
+        } else {
+            console.log("abgebrochen");
+        }
     });
-
-    //maybe some code follows here
-
 });
